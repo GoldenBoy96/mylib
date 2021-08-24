d471e989a2959f40b6c0c00c3dc64a3e547a5b67
Cậu Vàng <minhnphse160370@fpt.edu.vn>
2021-08-24 15:43:49 +0700

    Input

---
+++ TestLib.java
@@ -0,0 +1,15 @@
+
+package mylib;
+
+import mylib.util.Input;
+
+public class TestLib {
+
+    public static void main(String[] args) {
+        String a = Input.inputString("Hi em: ", "Yeu em!", "Lai di em: ");
+        System.out.println(a);
+    }
+    
+    
+    
+}
---
+++ util/Input.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mylib.util;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Huu Minh
+ */
+public class Input {
+
+    private static final Scanner SC = new Scanner(System.in);
+
+    public static int inputInteger(String inputMsg, String outputMsg, String reInputMsg) {
+        int input = 0;
+
+        System.out.print(inputMsg);
+        while (true) {
+            try {
+                input = Integer.parseInt(SC.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.print(reInputMsg);
+            }
+        }
+        outputMsg = outputMsg.trim();
+        if (!outputMsg.equals("")) {
+            System.out.println(outputMsg);
+        }
+        return input;
+    }
+
+    public static int inputInteger(String inputMsg, String outputMsg, String reInputMsg, int bottomValue, int topValue) {
+        int input = 0;
+        if (bottomValue > topValue) {
+            int t = bottomValue;
+            bottomValue = topValue;
+            topValue = t;
+        }
+        System.out.print(inputMsg);
+        while (true) {
+            try {
+                input = Integer.parseInt(SC.nextLine());
+                if (bottomValue != topValue) { //default infinity
+                    if (input >= bottomValue && input <= topValue) {
+                        break;
+                    } else {
+                        System.out.print("Please input an integer between " + bottomValue + " and " + topValue + ": ");
+                    }
+                }
+                break;
+            } catch (NumberFormatException e) {
+                System.out.print(reInputMsg);
+            }
+        }
+        outputMsg = outputMsg.trim();
+        if (!outputMsg.equals("")) {
+            System.out.println(outputMsg);
+        }
+        return input;
+    }
+
+    public static double inputDouble(String inputMsg, String outputMsg, String reInputMsg) {
+        double input = 0;
+
+        System.out.print(inputMsg);
+        while (true) {
+            try {
+                input = Double.parseDouble(SC.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.print(reInputMsg);
+            }
+        }
+        outputMsg = outputMsg.trim();
+        if (!outputMsg.equals("")) {
+            System.out.println(outputMsg);
+        }
+        return input;
+    }
+
+    public static double inputDouble(String inputMsg, String outputMsg, String reInputMsg, double bottomValue, double topValue) {
+        double input = 0;
+        if (bottomValue > topValue) {
+            double t = bottomValue;
+            bottomValue = topValue;
+            topValue = t;
+        }
+        System.out.print(inputMsg);
+        while (true) {
+            try {
+                input = Double.parseDouble(SC.nextLine());
+                if (bottomValue != topValue) { //default infinity
+                    if (input >= bottomValue && input <= topValue) {
+                        break;
+                    } else {
+                        System.out.print("Please input an Double between " + bottomValue + " and " + topValue + ": ");
+                    }
+                }
+                break;
+            } catch (NumberFormatException e) {
+                System.out.print(reInputMsg);
+            }
+        }
+        outputMsg = outputMsg.trim();
+        if (!outputMsg.equals("")) {
+            System.out.println(outputMsg);
+        }
+        return input;
+    }
+    
+    public static String inputString(String inputMsg, String outputMsg, String reInputMsg) {
+        String input = null;
+        System.out.print(inputMsg);
+        while (true) {
+            try {
+                input = SC.nextLine();
+                break;               
+            } catch (NumberFormatException e) {
+                System.out.print(reInputMsg);
+            }
+        }
+        outputMsg = outputMsg.trim();
+        if (!outputMsg.equals("")) {
+            System.out.println(outputMsg);
+        }
+        return input;
+    }
+    
+}
